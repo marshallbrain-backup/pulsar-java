faa72ec103ecf221e7e03153db6cee8244047303
marshallbrain <marshalldbrain@gmail.com>
2020-02-08 10:59:30 -0500

    Add time passing buttons and border layout panel

--- src/main/kotlin/ui/colony/Colony.kt
+++ src/main/kotlin/ui/colony/Colony.kt
@@ -5,6 +5,10 @@ import com.marshalldbrain.pulsar.colony.districts.DistrictType
 import com.marshalldbrain.pulsar.resources.Resource
 import com.marshalldbrain.pulsar.resources.ResourceType
 import ui.colony.construction.construction
+import ui.colony.construction.updateAllocation
+import java.awt.BorderLayout
+import java.awt.Button
+import java.awt.Panel
 import javax.swing.JFrame
 import javax.swing.JPanel
 import javax.swing.JTabbedPane
@@ -16,23 +20,39 @@ fun main() {
 	val colony = Colony(initDistrictTypes())
 	
 	val frame = JFrame()
+	val panel = Panel(BorderLayout())
+	frame.add(panel)
+	
+	val timeControlPanel = Panel()
+	panel.add(timeControlPanel, BorderLayout.PAGE_START)
+	
+	val time1Day = Button("1 Day")
+	time1Day.addActionListener { passTime(1) }
+	timeControlPanel.add(time1Day)
+	val time30Day = Button("30 Day")
+	time30Day.addActionListener { passTime(30) }
+	timeControlPanel.add(time30Day)
 	
 	val tabs = JTabbedPane()
-	
 	tabs.addTab("Overview", JPanel())
 	tabs.addTab("Construction", construction(colony))
 	tabs.addTab("Manage Developments", JPanel())
 	tabs.addTab("Manage Population", JPanel())
-	
 	tabs.selectedIndex = 1
-	
-	frame.add(tabs)
+	panel.add(tabs, BorderLayout.CENTER)
 	
 	frame.pack()
 	frame.setLocationRelativeTo(null);
 	frame.isVisible = true
 	frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
 
+}
+
+fun passTime(time: Int) {
+	
+	colony.constructionManager.tick(time)
+	updateAllocation()
+	
 }
 
 private fun initDistrictTypes() : Set<DistrictType> {
